8ed5dec8db77a311e3bac51ef86d2b5ca9833047
skyline-nju <duanyu.nju@hotmail.com>
2024-02-07 19:47:08 +0100

    implement various time integration schemes

--- NRQS/NRQS.py
+++ NRQS/NRQS.py
@@ -3,30 +3,34 @@ import matplotlib.pyplot as plt
 
 
 
-def plot_profile(rho, px, dx, t):
+def plot_profile(rho, px, dx, t, axes=None, linestyle="-"):
     rho_x = np.mean(rho, axis=1)
     px_x = np.mean(px, axis=1)
     x = np.arange(rho_x[0].size) * dx + dx / 2
     Lx = x[-1] + dx/2
 
-    figsize = (8, 3)
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=figsize, constrained_layout=True)
+    if axes is None:
+        figsize = (8, 3)
+        fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=figsize, constrained_layout=True)
+    else:
+        ax1, ax2 = axes
 
-    ax1.plot(x, rho_x[0], label=r"$S=A$")
-    ax1.plot(x, rho_x[1], label=r"$S=B$")
-    ax2.plot(x, px_x[0], label=r"$S=A$")
-    ax2.plot(x, px_x[1], label=r"$S=B$")
+    ax1.plot(x, rho_x[0], label=r"$S=A$", linestyle=linestyle)
+    ax1.plot(x, rho_x[1], label=r"$S=B$", linestyle=linestyle)
+    ax2.plot(x, px_x[0], label=r"$S=A$", linestyle=linestyle)
+    ax2.plot(x, px_x[1], label=r"$S=B$", linestyle=linestyle)
     ax1.set_xlim(0, Lx)
+
     ax2.set_xlabel(r"$x$")
     ax1.set_ylabel(r"$\langle\rho_{S}(\mathbf{x}, t) \rangle_y $")
     ax2.set_ylabel(r"$\langle p_{x,S}(\mathbf{x}, t) \rangle_y $")
+    if axes is None:
+        ax1.legend()
+        ax2.legend()
 
-    ax1.legend()
-    ax2.legend()
-
-    fig.suptitle(r"$t=%g$" % t)
-    plt.show()
-    plt.close()
+        plt.suptitle(r"$t=%g$" % t)
+        plt.show()
+        plt.close()
 
 
 def show_fields(phi, px, py, t, spacing):
@@ -129,20 +133,77 @@ def get_one_frame(fin, fout, i_frame=-1):
     np.savez_compressed(fout, t_arr=t_arr, rho_arr=rho_arr, px_arr=px_arr, py_arr=py_arr)
 
 
+
+def compare_profiles():
+    # i_frame = 64
+    i_frame = 108
+
+    fins = [r"data/L12.8_1.6_Dr0.100_Dt0.01_e0.000_0.000_J1.000_-1.000_dx0.1_h0.0005_s2100.npz",  # full nonlinearity
+            r"data/L12.8_1.6_Dr0.100_Dt0.01_e0.000_0.000_J1.000_-1.000_dx0.1_h0.0005_s2101.npz"   # full nonlinearity, de-aliased
+            ]
+    dx = 0.1
+    fig, axes = plt.subplots(2, 1, sharex=True, sharey=True, constrained_layout=True)
+    linestyles = ["-", ":"]
+
+    for i, fin in enumerate(fins):
+        with np.load(fin, "r") as data:
+            t_arr = data["t_arr"]
+            rho_arr = data["rho_arr"]
+            px_arr = data["px_arr"]
+            py_arr = data["py_arr"]
+            plot_profile(rho_arr[i_frame], px_arr[i_frame], dx, t_arr[i_frame], axes=axes, linestyle=linestyles[i])
+    axes[0].legend()
+    axes[1].legend()
+
+    plt.suptitle(r"$t=%g$" % t_arr[i_frame])
+    plt.show()
+    plt.close()
+
+
+def show_rho_min_varied_1st_schemes():
+    fins = [r"data/L12.8_1.6_Dr0.100_Dt0.01_e0.000_0.000_J1.000_-1.000_dx0.1_h0.0005_s2104.npz",  # RK4, full nonlinearity
+            # r"data/L12.8_1.6_Dr0.100_Dt0.01_e0.000_0.000_J1.000_-1.000_dx0.1_h0.0005_s2102.npz",  # partial nonlinearity
+            r"data/L12.8_1.6_Dr0.100_Dt0.01_e0.000_0.000_J1.000_-1.000_dx0.1_h0.0005_s2100.npz",  # full nonlinearity
+            r"data/L12.8_1.6_Dr0.100_Dt0.01_e0.000_0.000_J1.000_-1.000_dx0.1_h0.0005_s2101.npz"   # full nonlinearity, de-aliased
+            ]
+    labels = ["RK4, full nonlinearity", "Euler, full nonlinearity", "Euler, full nonlinearity, de-aliased"]
+
+    fig, ax = plt.subplots(1, 1, constrained_layout=True)
+    for i, fin in enumerate(fins):
+        with np.load(fin, "r") as data:
+            t_arr = data["t_arr"]
+            rho_arr = data["rho_arr"]
+            px_arr = data["px_arr"]
+            py_arr = data["py_arr"]
+            rho_min = np.array([i.min() for i in rho_arr])
+            ax.plot(t_arr, rho_min, label=labels[i])
+    ax.set_xlabel(r"$t$")
+    ax.set_ylabel(r"${\rm min}(\rho)$")
+    ax.set_xlim(0)
+    # ax.set_xscale("log")
+    # ax.set_yscale("log")
+
+    ax.legend()
+    plt.show()
+    plt.close()
+
+
+
+
 if __name__ == "__main__":
     dx = 0.1
-    dt = 1e-3
+    dt = 5e-4
 
     Lx = 12.8
     Ly = 1.6
 
-    Dr = 1
-    Dt = 0.02
+    Dr = 0.1
+    Dt = 0.01
 
-    eta_AA = eta_BB = -2
-    eta_AB = 0.1
+    eta_AA = eta_BB = 0
+    eta_AB = 1
     eta_BA = -eta_AB
-    seed = 111
+    seed = 2100
 
     fnpz = f"data/L{Lx:g}_{Ly:g}_Dr{Dr:.3f}_Dt{Dt:g}_e{eta_AA:.3f}_{eta_BB:.3f}_J{eta_AB:.3f}_{eta_BA:.3f}_dx{dx:g}_h{dt:g}_s{seed}.npz"
     with np.load(fnpz, "r") as data:
@@ -151,19 +212,22 @@ if __name__ == "__main__":
         px_arr = data["px_arr"]
         py_arr = data["py_arr"]
 
-        i_frame = 89
+        i_frame = 0
         show_fields(rho_arr[i_frame], px_arr[i_frame], py_arr[i_frame], t_arr[i_frame], dx)
         plot_profile(rho_arr[i_frame], px_arr[i_frame], dx, t_arr[i_frame])
         
-        # for i, t in enumerate(t_arr):
-        #     show_fields(rho_arr[i], px_arr[i], py_arr[i], t, 0.05)
+    #     # for i, t in enumerate(t_arr):
+    #     #     show_fields(rho_arr[i], px_arr[i], py_arr[i], t, 0.05)
 
 
-        show_space_time_densities(rho_arr, dx, t_arr)
+    show_space_time_densities(rho_arr, dx, t_arr)
+    
+    show_rho_min_varied_1st_schemes()
 
-        # plt.plot(t_arr, rho_arr[])
+    compare_profiles()
 
-    # fin = r"data\L12.8_1.6_Dr1.000_Dt0.02_e-2.000_-2.000_J0.100_-0.100_dx0.1_h0.001_s223.npz"
-    # fout = r"data\L12.8_1.6_Dr1.000_Dt0.02_e-2.000_-2.000_J0.100_-0.100_dx0.1_h0.001_s111.npz"
+
+    # fin = r"data\L12.8_1.6_Dr0.100_Dt0.01_e0.000_0.000_J1.000_-1.000_dx0.1_h0.001_s100.npz"
+    # fout = r"data\L12.8_1.6_Dr0.100_Dt0.01_e0.000_0.000_J1.000_-1.000_dx0.1_h0.001_s4100.npz"
     # get_one_frame(fin, fout)
 
